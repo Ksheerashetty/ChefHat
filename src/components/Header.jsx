8d5269340e20c67dc1f11a651c4093293b53cd7c
Ksheera Shetty <ksheerashetty@gmail.com>
2025-07-28 00:06:53 +0530

    image update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
+import chefImage from '/src/assets/robo.png';
 export default function Header() {
   return (
     <>
     <header>
-        <img src="/src/assets/robo.png" alt="Chef Hat Logo" />
+        <img src={chefImage} alt="Chef Hat Logo" />
       <h1>Welcome to ChefHat</h1>
     </header>
     </>
